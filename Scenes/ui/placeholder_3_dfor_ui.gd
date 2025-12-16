5fb8b8268b5a5ffe6b095595bb8b76879fd6f33a
Jesse Carnaxide <jcarnaxide@gmail.com>
2025-12-16 08:28:55 -0500

    wip on moving camera

--- Scenes/ui/placeholder_3_dfor_ui.gd
+++ Scenes/ui/placeholder_3_dfor_ui.gd
@@ -1,4 +1,10 @@
 extends Node3D
 
-func _on_button_pressed() -> void:
-	GameState.current = GameState.State.PLAY
+@onready var main_menu_camera: Camera3D = $MainMenuCamera
+
+
+func on_game_state_changed(new_state: GameState.State, old_state: GameState.State):
+	if new_state == GameState.State.PEN and old_state == GameState.State.MAIN_MENU:
+		var tween = create_tween()
+		tween.set_parallel()
+		tween.tween_property()
