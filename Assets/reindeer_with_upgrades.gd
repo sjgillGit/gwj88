19bba63d202fbe76470cadd3ed958504e3e4e15b
Jesse Carnaxide <jcarnaxide@gmail.com>
2025-12-16 23:53:39 -0500

    update reindeer model on upgrade

--- Assets/reindeer_with_upgrades.gd
+++ Assets/reindeer_with_upgrades.gd
@@ -4,3 +4,27 @@ func set_run_speed(value: float):
 	var at := %AnimationTree
 	at.set("parameters/running/blend_amount", absf(value))
 	at.set("parameters/run_timescale/scale", value * 3)
+
+
+func _ready() -> void:
+	_update_upgrades()
+	DeerUpgrades.upgrades_updated.connect(_update_upgrades)
+
+
+func _get_upgrade_node_3ds() -> Array[Upgrade]:
+	var found: Array[Upgrade]
+	for child in find_children("*", "Node3D"):
+		if child is Upgrade:
+			found.append(child)
+	return found
+
+
+func _update_upgrades():
+	var upgrades = DeerUpgrades.get_upgrades()
+	for child in _get_upgrade_node_3ds():
+		if child.category in upgrades:
+			if child.category == DeerUpgrades.Category.SMALL_ANTLERS \
+			and DeerUpgrades.Category.LARGE_ANTLERS in upgrades:
+				child.hide()
+			else:
+				child.show()
--- Scripts/singletons/autoloads/deer_upgrades.gd
+++ Scripts/singletons/autoloads/deer_upgrades.gd
@@ -1,5 +1,6 @@
-class_name DeerUpgrades
-extends RefCounted
+extends Node
+
+signal upgrades_updated
 
 enum Category {
 	BASE,
@@ -16,26 +17,22 @@ enum Category {
 	# TRAIL, # Todo with assets, rainbow road
 }
 
-static var _upgrades: Array[Category]
+var _upgrades: Array[Category] = [Category.BASE]
 
 
-static func increment_upgrade():
+func increment_upgrade():
 	for upgrade in DeerUpgrades.Category.values():
 		if upgrade in _upgrades:
 			continue
 		_upgrades.append(upgrade)
 		print("Adding upgrade: %s" % Category.keys()[upgrade])
+		upgrades_updated.emit()
 		break
 
 
-static func upgrade_available_for_purchase() -> bool:
+func upgrade_available_for_purchase() -> bool:
 	return len(_upgrades) < len(Category.keys())
 
 
-#static func set_upgrade(category: Category):
-	#print("Adding upgrade: %s" % Category.keys()[category])
-	#_upgrades.append(category)
-
-
-static func get_upgrades():
+func get_upgrades():
 	return _upgrades
