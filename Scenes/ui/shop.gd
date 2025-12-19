d2f59ae5743cdad3bbf1d01947e9e4e0467720fa
bergie <bergiebird@gmail.com>
2025-12-19 18:45:24 -0500

    Shop Ordered

--- Scenes/ui/shop.gd
+++ Scenes/ui/shop.gd
@@ -7,8 +7,14 @@ const POSITION_STARTING: Vector2 = Vector2(5.0, 2.0)
 const POSITION_FINAL: Vector2 = Vector2(-222.0, 2.0)
 
 var tween: Tween
+var upgrades: Array[UiShopButton]
+
+
 
 func _ready() -> void:
+	for child in get_children():
+		if child is UiShopButton:
+			upgrades.append(child)
 	_return_to_start()
 
 func _return_to_start() -> void:
@@ -23,3 +29,7 @@ func _on_ui_pen_visibility_changed() -> void:
 	if visible:
 		tween = create_tween()
 		tween.tween_property(self, "position", POSITION_FINAL, 1.16).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+
+
+func _disable_button() -> void:
+	pass
