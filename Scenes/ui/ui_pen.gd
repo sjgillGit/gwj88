ede8cb0b673d83ee8930cfc52b6368ab1d9804cd
Jesse Carnaxide <jcarnaxide@gmail.com>
2025-12-20 01:31:50 -0500

    setup some money tracking

--- Scenes/ui/ui_pen.gd
+++ Scenes/ui/ui_pen.gd
@@ -2,7 +2,9 @@ class_name UiPen
 extends Control
 
 @onready var upgrade_button: TextureButton = $UpgradeButton
+@onready var money_label = %MoneyLabel
 
+var orig_money_label_text: String
 
 func _update_button_enablement() -> void:
 	if not DeerUpgrades.upgrade_available_for_purchase():
@@ -16,3 +18,14 @@ func _on_play_button_pressed() -> void:
 func _on_upgrade_button_pressed() -> void:
 	DeerUpgrades.increment_upgrade()
 	_update_button_enablement()
+
+
+func _update_money_label():
+	var count = GameState.money
+	money_label.text = orig_money_label_text.replace("{0}", str(count))
+
+
+func _ready():
+	orig_money_label_text = money_label.text
+	_update_money_label()
+	GameState.money_changed.connect(_update_money_label)
--- Scenes/ui/ui_play.gd
+++ Scenes/ui/ui_play.gd
@@ -1,16 +1,41 @@
 class_name UiPlay
 extends Control
 
+@onready var flight_money_label = %FlightMoney
+@onready var roll_money_label = %RollMoney
+@onready var main_v_box = %MainVBox
+
+var orig_flight_money_label_text: String
+var orig_roll_money_label_text: String
+
 const FlightState = preload("res://Scripts/flight_state.gd").FlightState
 
 var flight_state: FlightState = FlightState.PRE_FLIGHT:
 	set(value):
 		flight_state = value
-		$Button.visible = flight_state == FlightState.POST_FLIGHT
+		main_v_box.visible = flight_state == FlightState.POST_FLIGHT
+
+var flight_money: int:
+	set(v):
+		flight_money_label.text = orig_flight_money_label_text.replace(
+			"{0}",
+			str(v)
+		)
+		flight_money = v
+
+var roll_money: int:
+	set(v):
+		roll_money_label.text = orig_roll_money_label_text.replace(
+			"{0}",
+			str(v)
+		)
+		roll_money = v
 
 
 func _ready():
 	visibility_changed.connect(_visibility_changed)
+	orig_flight_money_label_text = flight_money_label.text
+	orig_roll_money_label_text = roll_money_label.text
 
 
 func _visibility_changed():
