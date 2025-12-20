a11c0dbca8c0b48b1381756b0cab341b631f3b91
Jamie Pate <jamie.pate@gmail.com>
2025-12-19 23:54:20 -0800

    Improve wind fx and strength numbers
    Add a bunch of example wind obstacles and a wind indicator graphic for
    when you're being pushed by wind

--- Scripts/environment/wind.gd
+++ Scripts/environment/wind.gd
@@ -1,60 +1,98 @@
 @tool
-extends Area3D
+extends DeerArea
 class_name Wind
 
 @onready var collision_shape: CollisionShape3D = %WindShape
 @onready var particles: GPUParticles3D = %WindParticles
+@onready var snow_particles: GPUParticles3D = %SnowParticles
 
 @export var area_size: Vector3 = Vector3.ONE:
 	set(value):
 		area_size = value
-		if is_node_ready() || Engine.is_editor_hint():
+		if is_node_ready():
 			_update_shape()
 
-@export var direction: Vector3 = Vector3.LEFT:
+@export var direction: Vector3 = Vector3.BACK:
 	set(value):
 		direction = value.normalized()
-		if is_node_ready() || Engine.is_editor_hint():
+		if is_node_ready():
 			_update_particles()
 
-@export_range(100.0, 10000.0, 1.0) var strength: float = 100.0:
+@export_range(0.0, 10000.0, 1.0) var strength: float = 100.0:
 	set(value):
 		strength = value
-		if is_node_ready() || Engine.is_editor_hint():
+		if is_node_ready():
 			_update_particles()
 
 @export_range(0.1, 2.0, 0.1) var particle_speed_multiplier: float = 1.0:
 	set(value):
 		particle_speed_multiplier = value
-		if is_node_ready() || Engine.is_editor_hint():
+		if is_node_ready():
 			_update_particles()
 
 func _ready() -> void:
-	await get_tree().process_frame
-	_update_shape()
-	_update_particles()
+	area_size = area_size
+	direction = direction
+	strength = strength
+	particle_speed_multiplier = particle_speed_multiplier
+	super()
+
+
+func get_global_wind_direction():
+	return direction * global_transform.basis
+
 
 func _physics_process(_delta: float) -> void:
 	for body in get_overlapping_bodies():
 		if body is RigidBody3D:
-			body.apply_central_force(direction * strength)
+			body.apply_central_force(direction * strength * body.mass)
+			if body is DeerMissile:
+				for p in [particles, snow_particles]:
+					p.global_position = body.global_position + body.linear_velocity.clampf(-25, 25)
+					p.global_position = body.global_position + body.linear_velocity.clampf(-25, 25)
 
 func _update_shape() -> void:
 	if collision_shape.shape is BoxShape3D:
 		collision_shape.shape.size = area_size
+	$FogVolume.size = area_size
 
 func _update_particles() -> void:
 	if not particles:
 		return
-
-	var mat := particles.process_material
-	if mat is ParticleProcessMaterial:
+	snow_particles.amount_ratio = clampf(0.10 + strength / 100.0, 0.0, 1.0)
+	for p: GPUParticles3D in [particles, snow_particles]:
+		var mat := p.process_material
 		# Match emission volume to wind area
-		mat.emission_box_extents = area_size * 0.5
 
 		# Push particles in wind direction
 		mat.direction = direction
-		mat.initial_velocity_min = strength * 0.01
-		mat.initial_velocity_max = strength * 0.02
+		var multiplier := 10.0 if p == particles else 1.0
+		mat.initial_velocity_min = strength * multiplier
+		mat.initial_velocity_max = strength * multiplier * 2
 
-		mat.turbulence_noise_strength = strength * 0.01
+		mat.turbulence_noise_strength = strength * 10
+	_reset_particle_sizes()
+
+func _reset_particle_sizes():
+	var aabb := AABB(Vector3(), Vector3())
+	aabb = aabb.expand(area_size)
+	aabb = aabb.expand(area_size * -1)
+	for p: GPUParticles3D in [particles, snow_particles]:
+		p.visibility_aabb = aabb
+		var mat := p.process_material as ParticleProcessMaterial
+		mat.emission_box_extents = area_size * 0.5
+
+func _on_body_entered(body: Node3D) -> void:
+	super(body)
+	var emit_size = 25
+	if body is DeerMissile:
+		for p in [particles, snow_particles]:
+			p.visibility_aabb = AABB(Vector3.ONE * -emit_size * 2, Vector3.ONE * emit_size * 4)
+			var mat := p.process_material as ParticleProcessMaterial
+			mat.emission_box_extents = Vector3.ONE * emit_size
+
+
+func _on_body_exited(body: Node3D) -> void:
+	super(body)
+	if !get_overlapping_bodies().any(func(b): return b is DeerMissile):
+		_reset_particle_sizes()
