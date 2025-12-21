34e68904f0d471d6d59400c41fadadf33a61af60
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2025-12-21 07:10:28 -0800

    Move targeting to snowman

--- Scenes/environment/snowman/snowball.gd
+++ Scenes/environment/snowman/snowball.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 
+@onready var gpu_particles_3d: GPUParticles3D = $GPUParticles3D
+@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+
 @export var speed: float = 100
 var target: Node3D
 
@@ -10,9 +13,16 @@ func init(speed: float, dir: Vector3) -> void:
 func _physics_process(_delta: float) -> void:
 	if target:
 		look_at(target.global_position)
-		velocity = -basis.z * speed
+		velocity = lerp(velocity, -basis.z * speed, 0.2)
 	move_and_slide()
 	for idx in get_slide_collision_count():
 		var collision = get_slide_collision(idx)
-		print(collision)
+		gpu_particles_3d.restart()
+		mesh_instance_3d.hide()
+		await get_tree().create_timer(3).timeout
 		queue_free()
+
+
+func _on_area_3d_body_entered(bodwdy: Node3D) -> void:
+	pass
+	#target = body
